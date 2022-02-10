160f27cab3e2994d15ef46a1661e88624ea97d17
Ivan Sena da Cruz <ivanprojetosti@gmail.com>
2022-02-09 22:25:27 -0300

    alerta para favoritar adicionado no icone de favoritos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async exibirALERTA() {
+    const alert = await this.alertController.create({
+      cssClass: 'alerta_favoritar',
+      header: 'Favoritar Filme',
+      message: 'Deseja favoritar esse filme ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'SIM, favoritar',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
 
 }
